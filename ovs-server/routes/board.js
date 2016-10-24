62cf2e88a97cfd61b9bb5ccfb234a5ec2c2134a3
iwin2471 <iwin12372@gmail.com>
2016-10-25 03:03:20 +0900

    start search

--- ovs-server/routes/board.js
+++ ovs-server/routes/board.js
@@ -55,7 +55,7 @@ router.post('/write', function(req, res) {
                 return res.status(406).send("DB error");
             } else if (result) {
 
-                var board = new Boards({
+                var board = new Board({
                     boardid: boardid,
                     board_writer: result.nick_name,
                     writer_img: result.profile_image,
@@ -70,7 +70,12 @@ router.post('/write', function(req, res) {
                       Tour.update({id: tourid}, {$push: {board_ids: boardid}}, function(err, result){
                           if(err) res.status(409).send("DB error");
                           else{
-                            return res.status(200).send(board);
+                            Tour.findOne({id: tourid}, function(err, tours){
+                              Users.update({token: token}, {$push: {visit: {name: tours.name, name_eng: tours.name_eng, img_url: tours.img_url}}}, function(err, results){
+                                if(err) res.status(409).send("DB error");
+                                else res.status(200).send(board);
+                              });
+                            });
                           }
                       });
                     }
@@ -126,8 +131,32 @@ router.post('/dislike', function(req, res) {
         }
       });
     }else{
-      return res.status(405).send("no boardx");
+      return res.status(405).send("no board");
     }
+  });
+});
+
+
+router.post('/addCommant', function(req, res){
+  var token = req.body.token;
+  var boardid = req.body.boardid;
+  var summary = req.body.summary;
+  var date = moment().format();
+
+  Board.findOne({boardid: boardid}, function(err, board) {
+    Users.findOne({token: token}, function(err, user) {
+      if(err) return res.status(409).send("DB error");
+      else if(user){
+        Board.update({boardid: boardid}, {$push: {comments:{writer: user.name, date: date, summary: summary, profile_image: user.profile_image}}}, function(err, result){
+          if(err) return res.status(409).send("DB error");
+          else{
+            return res.status(200).send(board);
+          }
+        });
+      }else{
+        return res.status(405).send("user not found");
+      }
+    });
   });
 });
 
